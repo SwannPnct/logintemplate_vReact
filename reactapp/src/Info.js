c1e16c0b9e55531dd85c67821144319e979142ac
Swann Pincet <swann.pincet@gmail.com>
2021-02-13 22:23:56 +0100

    finished redirection in regards user token after login

--- reactapp/src/Info.js
+++ reactapp/src/Info.js
@@ -1,13 +1,20 @@
 import React,{useContext, useState} from 'react'
 import {TokenContext} from './App'
+import {Redirect} from 'react-router-dom'
 
 export default function Info() {
     const {token, setToken} = useContext(TokenContext)
     const [userInfo, setUserInfo] = useState({})
 
-    return (
-        <div className="main_container">
-            <p>Hello</p>
-        </div>
-    )
+    if (!token) {
+        return (
+            <Redirect to='/'/>
+        )
+    } else {
+        return (
+            <div className="main_container">
+                <p>Hello</p>
+            </div>
+        )
+    }
 }
--- reactapp/src/Login.js
+++ reactapp/src/Login.js
@@ -1,5 +1,6 @@
 import React,{useContext, useState} from 'react'
 import {TokenContext} from './App'
+import {Redirect} from 'react-router-dom'
 
 export default function Login() {
     const {token, setToken} = useContext(TokenContext)
@@ -47,49 +48,57 @@ export default function Login() {
             setToken(resJson.token)
         }
     }
-
-    return (
-        <div className="main_container">
-            <div className="login_forms">
-                <div className="gen_form">
-                    <div className="form_header">
-                        <h3>Login into your account</h3>
+    
+    if (token) {
+        return (
+            <Redirect to='/info'/>
+        )
+    } else {
+        return (
+            <div className="main_container">
+                <div className="login_forms">
+                    <div className="gen_form">
+                        <div className="form_header">
+                            <h3>Login into your account</h3>
+                        </div>
+                        <div className="form">
+                        {errorSI ? <div className="alert_message">{errorSI}</div> : null}
+                            <div>
+                                <label>Email</label>
+                                <input type="text" onChange={() => e => setEmailSI(e.target.value)} value={emailSI}></input>
+                            </div>
+                            <div>
+                                <label>Password</label>
+                                <input type="password" onChange={() => e => setPasswordSI(e.target.value)} value={passwordSI}></input>
+                            </div>
+                            <button class="submit_button" onClick={() => handleSignIn()}>Sign-In</button>
+                        </div>
                     </div>
-                    <div className="form">
-                    {errorSI ? <div className="alert_message">{errorSI}</div> : null}
-                        <div>
-                            <label>Email</label>
-                            <input type="text" onChange={() => e => setEmailSI(e.target.value)} value={emailSI}></input>
+                    <div class="gen_form" id="form_two">
+                        <div class="form_header">
+                            <h3>Create a new account</h3>
                         </div>
-                        <div>
-                            <label>Password</label>
-                            <input type="password" onChange={() => e => setPasswordSI(e.target.value)} value={passwordSI}></input>
+                        <div className="form">
+                            {errorSU ? <div className="alert_message">{errorSU}</div> : null}
+                            <div>
+                                <label>Username</label>
+                                <input type="text" onChange={() => e => setUsernameSU(e.target.value)} value={usernameSU}></input>
+                            </div>
+                            <div>
+                                <label>Email</label>
+                                <input type="text" onChange={() => e => setEmailSU(e.target.value)} value={emailSU}></input>
+                            </div>
+                            <div>
+                                <label>Password</label>
+                                <input type="password" onChange={() => e => setPasswordSU(e.target.value)} value={passwordSU}></input>
+                            </div>
+                            <button class="submit_button" onClick={() => handleSignUp()}>Sign-Up</button>
                         </div>
-                        <button class="submit_button" onClick={() => handleSignIn()}>Sign-In</button>
-                    </div>
-                </div>
-                <div class="gen_form" id="form_two">
-                    <div class="form_header">
-                        <h3>Create a new account</h3>
-                    </div>
-                    <div className="form">
-                        {errorSU ? <div className="alert_message">{errorSU}</div> : null}
-                        <div>
-                            <label>Username</label>
-                            <input type="text" onChange={() => e => setUsernameSU(e.target.value)} value={usernameSU}></input>
-                        </div>
-                        <div>
-                            <label>Email</label>
-                            <input type="text" onChange={() => e => setEmailSU(e.target.value)} value={emailSU}></input>
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <input type="password" onChange={() => e => setPasswordSU(e.target.value)} value={passwordSU}></input>
-                        </div>
-                        <button class="submit_button" onClick={() => handleSignUp()}>Sign-Up</button>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+    
 }
